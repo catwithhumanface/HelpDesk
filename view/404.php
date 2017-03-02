06574d96237b3a04f285e94301cdee72633b88b1
elshobokshy <islam20088@hotmail.com>
2017-03-02 12:56:21 +0100

    Modified not found page's UI

--- view/404.php
+++ view/404.php
@@ -1,7 +1,20 @@
 <?php require 'shared/header.php' ?>
 
-<h1>How did you end up here? :/</h1>
-
-<p>Sorry, this page isn't available... Stop messing with the site's URLs!!!!</p>
+<div class="content">
+    <div class="container">
+        <div class="l_g">
+            <div class="col-md-12 praesent">
+                <div class="l_g_r">
+                    <div class="dapibus">
+                        <h2>How did you end up here? :/</h2>
+                        <br/>
+                        <h2 class="addFirstPost">Sorry, this page isn't available... Stop messing with the site's URLs!!!!</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+</div>
 
 <?php require 'shared/footer.php' ?>
