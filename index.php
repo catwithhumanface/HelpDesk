0038aa9296b5b73b68896c90a11ea9bf8f06687d
elshobokshy <islam20088@hotmail.com>
2017-03-02 12:33:43 +0100

    Removed unecessary test used for dev

--- index.php
+++ index.php
@@ -27,29 +27,23 @@ define('ROOT_URL', PROTOCOL . $_SERVER['HTTP_HOST'] . str_replace('\\', '', dirn
 define('ROOT_HOME', str_replace('\\', '', dirname(htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES))) . '/');
 define('ROOT_PATH', __DIR__ . '/');
 
+require ROOT_PATH . 'app/Autoloader.php';
+app\Autoloader::init(); // Load necessary classes
 
-try {
-    require ROOT_PATH . 'app/Autoloader.php';
-    app\Autoloader::init(); // Load necessary classes
-	
-	if(!empty($_GET['p'])) {
-		$controller = $_GET['p'];
-	} else {
-		$controller = 'blogController';
-	}
-	
-	if(!empty($_GET['a'])){
-		$action = $_GET['a'];
-	} else {
-		$action = 'home';
-	}
-	
-    $params = [
-		'ctrl' => $controller, 
-		'act' => $action
-	];
-    app\Router::run($params);
-	
-} catch (\Exception $e) {
-    echo $e->getMessage();
+if(!empty($_GET['p'])) {
+	$controller = $_GET['p'];
+} else {
+	$controller = 'blogController';
 }
+
+if(!empty($_GET['a'])){
+	$action = $_GET['a'];
+} else {
+	$action = 'home';
+}
+
+$params = [
+	'ctrl' => $controller, 
+	'act' => $action
+];
+app\Router::run($params);
