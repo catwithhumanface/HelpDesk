3893b2933624d192bde5b4452785e94a049011dd
zinsen <74664057+zinsen@users.noreply.github.com>
2021-04-28 16:04:16 +0200

    Update BlogController.php

--- controller/BlogController.php
+++ controller/BlogController.php
@@ -174,7 +174,7 @@ class BlogController {
 
 		/**
 		 * Generation of the subscription page.
-		 */
+		 
 		public function subscription() {
             $this->modelAuthentication = new Authentication();
 				if (!empty($_SESSION)) {
@@ -192,6 +192,92 @@ class BlogController {
 				}
 				$this->manager->getView('subscription');
 		}
+	
+		 */
+	
+		
+		public function subscription() {
+			
+	
+       		 $data = [
+            'username' => '',
+            'email' => '',
+            'password' => '',
+            'confirmPassword' => '',
+            'usernameError' => '',
+            'emailError' => '',
+            'passwordError' => '',
+            'confirmPasswordError' => ''
+        	];
+
+      	if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        // Process form
+        // Sanitize POST data
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+              $data = [
+                'username' => trim($_POST['username']),
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'usernameError' => '',
+                'emailError' => '',
+                'passwordError' => '',
+            ];
+
+            $nameValidation = "/^[a-zA-Z0-9]*$/";
+            $passwordValidation = "/^(.{0,7}|[^a-z]*|[^\d]*)$/i";
+
+            //Validate username on letters/numbers
+            if (empty($data['username'])) {
+                $data['usernameError'] = 'Please enter username.';
+            } elseif (!preg_match($nameValidation, $data['username'])) {
+                $data['usernameError'] = 'Name can only contain letters and numbers.';
+            }
+
+            //Validate email
+            if (empty($data['email'])) {
+                $data['emailError'] = 'Please enter email address.';
+            } elseif (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+                $data['emailError'] = 'Please enter the correct format.';
+            } 
+
+           // Validate password on length, numeric values,
+            if(empty($data['password'])){
+              $data['passwordError'] = 'Please enter password.';
+            } elseif(strlen($data['password']) < 6){
+              $data['passwordError'] = 'Password must be at least 8 characters';
+            } elseif (preg_match($passwordValidation, $data['password'])) {
+              $data['passwordError'] = 'Password must be have at least one numeric value.';
+            }
+
+            //Validate confirm password
+             if (empty($data['confirmPassword'])) {
+                $data['confirmPasswordError'] = 'Please enter password.';
+            } else {
+                if ($data['password'] != $data['confirmPassword']) {
+                $data['confirmPasswordError'] = 'Passwords do not match, please try again.';
+                }
+            }
+
+            // Make sure that errors are empty
+            if (empty($data['usernameError']) && empty($data['emailError']) && empty($data['passwordError']) && empty($data['confirmPasswordError'])) {
+
+                // Hash password
+                $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+
+                //Register user from model function
+                if ($this->manager->subscription($data)) {
+                    //Redirect to the login page
+                    header('location: ' . URLROOT . '/users/login');
+                } else {
+                    die('Something went wrong.');
+                }
+            }
+        }
+        $this->manager->getView('subscription', $data);
+    }
+	
+	
 
     /**
      * Generation of the logout page.
